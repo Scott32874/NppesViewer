ecdafeaa379e01e82e97c5cb50891ce2e36ece02
Scott Adams <scottadams@scotts-macbook-pro.local>
2015-11-15 10:20:44 -0500

    Initial Commit

---
+++ NppesViewerTests/NppesViewerTests.swift
@@ -0,0 +1,36 @@
+//
+//  NppesViewerTests.swift
+//  NppesViewerTests
+//
+//  Created by Scott Adams on 11/15/15.
+//  Copyright © 2015 Scott Adams. All rights reserved.
+//
+
+import XCTest
+@testable import NppesViewer
+
+class NppesViewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
